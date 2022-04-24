66a38a7112d12ebe8b2b265f7c655071948166ab
Éva Hartung <hartung.eva@edu.bme.hu>
2022-04-25 00:46:39 +0200

    Fixed CatServiceTest

--- app/src/test/java/hu/bme/aut/android/catviewer/network/CatServiceTest.kt
+++ app/src/test/java/hu/bme/aut/android/catviewer/network/CatServiceTest.kt
@@ -5,6 +5,7 @@ import hu.bme.aut.android.catviewer.model.Cat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -35,7 +36,7 @@ class DisneyServiceTest : ApiAbstract<CatService>() {
 
         assertThat(responseBody[0].id, `is`("6ft"))
         assertThat(responseBody[0].url, `is`("https://cdn2.thecatapi.com/images/6ft.jpg"))
-        assertThat(responseBody[0].width, `is`("500"))
-        assertThat(responseBody[0].height, `is`("332"))
+        assertThat(responseBody[0].width, `is`(equalTo(500)))
+        assertThat(responseBody[0].height, `is`(equalTo(332)))
     }
 }
