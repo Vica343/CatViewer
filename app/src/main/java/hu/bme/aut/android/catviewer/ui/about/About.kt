0e3ae914d8c6952528587471b90f4e893209b0ab
Éva Hartung <hartung.eva@edu.bme.hu>
2022-05-07 16:54:04 +0200

    About page

--- app/src/main/java/hu/bme/aut/android/catviewer/ui/about/About.kt
+++ app/src/main/java/hu/bme/aut/android/catviewer/ui/about/About.kt
@@ -1,18 +1,26 @@
 package hu.bme.aut.android.catviewer.ui.about
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import android.content.Context
+import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.rounded.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import hu.bme.aut.android.catviewer.ui.main.NavScreen
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.coil.CoilImage
+import hu.bme.aut.android.catviewer.R
+import hu.bme.aut.android.catviewer.ui.main.Cats
 
 @Composable
 fun About(
@@ -20,24 +28,46 @@ fun About(
     pressOnBack: () -> Unit = {}
     )
 {
-    TopAppBar(
-        title = { Text(text = "CatViewer") },
-        actions = {
-            IconButton(
-                onClick = {
-                    pressOnBack()
-                },
-                modifier = Modifier.padding(
-                    horizontal = 20.dp
-                )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "CatViewer") },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            pressOnBack()
+                        },
+                        modifier = Modifier.padding(
+                            horizontal = 20.dp
+                        )
+                    )
+                    {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Icon",
+                            tint = Color.White
+                        )
+                    }
+                }
             )
-            {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "Icon",
-                    tint = Color.White
+        }  ,
+        content = {
+            val context = LocalContext.current
+            Spacer(modifier = Modifier.height(20.dp))
+            Column( horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(painter = painterResource(R.drawable.catlogo),
+                    modifier = Modifier
+                        .size(500.dp),
+                    contentDescription ="cat viewer logo" )
+                Text(
+                    text = viewModel.copyright,
+                    fontSize = 17.sp,
+                    textAlign = TextAlign.Center
                 )
             }
-        }
-    )
+
+        })
+
+
+
 }
--- app/src/main/java/hu/bme/aut/android/catviewer/ui/about/AboutViewModel.kt
+++ app/src/main/java/hu/bme/aut/android/catviewer/ui/about/AboutViewModel.kt
@@ -2,11 +2,14 @@ package hu.bme.aut.android.catviewer.ui.about
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import android.content.pm.PackageInfo;
+import hu.bme.aut.android.catviewer.BuildConfig
+import hu.bme.aut.android.catviewer.CatViewer
+import hu.bme.aut.android.catviewer.R
 
 
 @HiltViewModel
 class AboutViewModel @Inject constructor(
-    private val aboutRepository: AboutRepository
 ) : ViewModel(){
-    // TODO
+    val copyright = "App version v." + BuildConfig.VERSION_NAME + "\nAll rights reserved\n2022 \u00a9"
 }
